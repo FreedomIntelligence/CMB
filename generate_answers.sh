490db861a06b1ac28c94d71add415eca0393b669
g-h-chen <1050536621a@gmail.com>
2023-08-08 20:05:11 +0800

    update bash script

--- generate_answers.sh
+++ generate_answers.sh
@@ -1,17 +1,18 @@
-test_data='/mntcephfs/data/med/xidong/CMB/data/CMB-main/CMB-test/CMB-test-choice-question-merge.json' #不变
-# test_data='/mntcephfs/data/med/xidong/CMB/data/CMB-test-qa/CMB-test-qa.json' #不变
+test_data='./data/CMB-Exam/CMB-test/CMB-test-choice-question-merge.json' # 
+# test_data='./data/CMB-Clin/CMB-Clin-qa.json' #不变
 
 
 
-task_name='Zero-test-cot'
+task_name='Exam'
 
-model_id="huatuo"
-# cot
-nohup accelerate launch --gpu_ids='all' --main_process_port 27274 --config_file ./configs/accelerate_config.yaml  ./src/generate_answers.py \
+model_id="huatuo-chat"
+mkdir -p logs/${task_name}/
+
+accelerate launch --main_process_port 27274 --config_file ./configs/accelerate_config.yaml  ./src/generate_answers.py \
 --model_id=$model_id \
 --all_gather_freq=20 \
 --input_path=$test_data \
---output_path=/mntcephfs/data/med/xidong/CMB/result/${task_name}/${model_id}/modelans.json \
---cot_flag \ 
---model_config_path="./configs/model_config.yaml" > ./logs/${task_name}/${model_id}.log 2>&1 &
-
+--output_path=./result/${task_name}/${model_id}/modelans.json \
+--use_cot \
+--batch_size 1 \
+--model_config_path="./configs/model_config.yaml" 
