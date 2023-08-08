60d8e77a4f439f9b932cf6e557e1df3b3be160cf
g-h-chen <1050536621a@gmail.com>
2023-08-08 20:31:00 +0800

    update

--- score_exam.sh
+++
@@ -1,3 +0,0 @@
-modelans_path='/mntcephfs/data/med/xidong/CMB/result/Zero-exam-a/test/modelans.json'
-score_path='/mntcephfs/data/med/xidong/CMB/result/Zero-exam-a/test/score.json'
-python ./src/score_zhenti.py --modelans_path=${modelans_path} --score_path=${score_path}
